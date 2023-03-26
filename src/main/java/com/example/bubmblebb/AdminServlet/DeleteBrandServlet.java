1ccac0a55bccbe92686870ee825958104f94a752
Vimani <vimaniashika123@gmail.com>
2023-03-26 14:39:49 +0530

    Redirect to AdminBrandsServlet DeleteBrandServlet

--- src/main/java/com/example/bubmblebb/AdminServlet/DeleteBrandServlet.java
+++ src/main/java/com/example/bubmblebb/AdminServlet/DeleteBrandServlet.java
@@ -1,5 +1,7 @@
 package com.example.bubmblebb.AdminServlet;
 
+import com.example.bubmblebb.model.BrandDBUtil;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -9,6 +11,27 @@ import java.io.IOException;
 public class DeleteBrandServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String brandID = request.getParameter("id");
+
+        boolean isTrue;
+        isTrue = BrandDBUtil.deleteBrand(brandID);
+
+        if(isTrue == true)
+        {
+
+            // Redirect to AdminBrandsServlet
+            response.sendRedirect(request.getContextPath() + "/Brands");
+
+        }
+        else
+        {
+            response.setContentType("text/html");
+            response.getWriter().write("<script>alert('An error has occurred');</script>");
+
+            // Redirect to AdminBrandsServlet
+            response.sendRedirect(request.getContextPath() + "/Brands");
+
+        }
 
     }
 
