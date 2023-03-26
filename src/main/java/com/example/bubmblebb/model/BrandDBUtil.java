df9ca6fee4fa6f177ccb50e7d840732ff042c508
Vimani <vimaniashika123@gmail.com>
2023-03-26 14:11:16 +0530

    Function use to get All brand details

--- src/main/java/com/example/bubmblebb/model/BrandDBUtil.java
+++ src/main/java/com/example/bubmblebb/model/BrandDBUtil.java
@@ -1,4 +1,60 @@
 package com.example.bubmblebb.model;
 
+import com.example.bubmblebb.DB.DBConnection;
+import com.example.bubmblebb.entity.Brand;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BrandDBUtil {
+
+    private static boolean isSuccess = false;
+    private static Connection conn = null;
+    private  static Statement stmt = null;
+    private static ResultSet rs= null;
+
+
+    //Function use to get All brand details---------------------------------------------------------------------------------
+    public static List<Brand> getBrandList()
+    {
+        ArrayList<Brand> brands = new ArrayList<>();
+
+        try
+        {
+            conn = DBConnection.getConn();
+            stmt = conn.createStatement();
+
+            String sql = "select * from brands";
+            rs = stmt.executeQuery(sql);
+
+            while (rs.next())
+            {
+
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+
+                Brand brand = new Brand(id,name);
+
+                brands.add(brand);
+
+            }
+
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+
+        return  brands;
+
+    }
+    //End oif function use to get brand details-------------------------------------------------------------------------
+
+
+
+
+
 }
